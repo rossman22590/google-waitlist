5a7ed9e1bc0632b3bcc3b5f6e847fd63adc39a0c
Ross Cohen <rosscohen1990@gmail.com>
2023-03-27 00:12:15 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,27 @@ const phrases = [
 'can help you optimize your code for performance and scalability',
 'can explain how to use testing and debugging tools to improve your code',
 'can recommend resources and tools for staying up-to-date with the latest development trends and technologies',
+ 'Can provide one-on-one coding lessons to help you learn new languages and concepts',
+'Can suggest coding challenges and projects to help you practice what you've learned',
+'Can recommend online resources and tutorials to supplement your coursework',
+'Can offer guidance on how to build a portfolio of coding projects to showcase your skills to potential employers',
+'Can assist you in finding internship or job opportunities in the tech industry',
+'Can explain how to use open-source software and contribute to community projects',
+'Can provide feedback on your coding projects and offer suggestions for improvement',
+'Can help you prepare for coding interviews and technical assessments',
+'Can recommend coding bootcamps or workshops to help you accelerate your learning',
+'Can offer advice on how to balance your coding education with your other coursework and responsibilities',
+'Can help you create a study plan for your upcoming exams',
+'Can provide feedback on your college essays and papers',
+'Can suggest effective note-taking strategies for your classes',
+'Can explain how to use academic databases and sources for your research papers',
+'Can advise you on how to balance your coursework and extracurricular activities',
+'Can help you improve your public speaking and presentation skills',
+'Can offer tips on how to manage stress and maintain mental wellness during the school year',
+'Can assist you in preparing for internships and job interviews',
+'Can recommend resources and tools for improving your time management and productivity as a student',
+'Can provide guidance on how to network and build professional relationships in your field',
+
   
 ]
 
